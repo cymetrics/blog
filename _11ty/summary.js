0a08d6b338dcc4405a0547114d61a00cfd2c3460
Li Hu <li.hu@onedegree.hk>
2021-11-10 16:42:10 +0800

    add summary to og

--- _11ty/summary.js
+++ _11ty/summary.js
@@ -3,7 +3,7 @@ const hasSummary = content => content.split(tag).length === 3;
 const excerpt = (content) => content.split(tag)[1];
 const delHtmlTag = str => str.replace(/<[^>]+>/g,"");
 const contentInfo = content => {
-    const htmlPart = excerpt(content.post)
+    const htmlPart = excerpt(content)
     const startIndex = htmlPart.indexOf('<!--')
     const endIndex = htmlPart.indexOf('-->')
     return {
@@ -15,39 +15,20 @@ const contentInfo = content => {
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addFilter("excerpt", (content) =>
-      excerpt(content.post)
+      excerpt(content)
     );
 
-    eleventyConfig.addFilter("hasSummary", (content) =>
-      hasSummary(content.post)
-    );
+    eleventyConfig.addFilter("summary", function(content) {
+      if (!content || !hasSummary(content)) return ''
+      const {htmlPart, startIndex, endIndex} = contentInfo(content)
+      let summary = ''
 
-    eleventyConfig.addFilter("delHtmlTag", (content) =>
-      delHtmlTag(summary)
-    );
-
-    eleventyConfig.addShortcode("summary", function(content) {
-    if (!hasSummary(content.post)) return ''
-    const {htmlPart, startIndex, endIndex} = contentInfo(content)
-    let summary = ''
-
-    if( startIndex >= 0){
-      summary = htmlPart.slice(startIndex+4, endIndex)
-    } else {
-      summary = delHtmlTag(htmlPart)
-    }
-    summary = summary.trim().replace(/(\r\n\t|\n|\r\t)/g,"")
-    return `<div class="summary">${summary}</div>`
-    });
-
-    eleventyConfig.addShortcode("summaryLong", function(content) {
-    if (!hasSummary(content.post)) return ''
-    const {htmlPart, startIndex, endIndex} = contentInfo(content)
-    let summaryLong = htmlPart
-
-    if( startIndex >= 0){
-      summaryLong = htmlPart.slice(startIndex+4, endIndex)
-    } 
-    return `<div class="summaryLong">${summaryLong}</div>`
+      if( startIndex >= 0){
+        summary = htmlPart.slice(startIndex+4, endIndex)
+      } else {
+        summary = delHtmlTag(htmlPart)
+      }
+      summary = summary.trim().replace(/(\r\n\t|\n|\r\t)/g,"")
+      return `<div class="summary">${summary}</div>`
     });
 };
